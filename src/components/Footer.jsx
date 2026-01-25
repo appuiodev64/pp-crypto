55d8604403bd57e40b00910a68211fa20506e423
Adam Bakir <bakiradam1502@gmail.com>
2026-01-25 11:30:10 -0500

    Bruh

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ export default function Footer() {
   return (
 
     <footer className="text-center text-sm py-4 border-t bg-white dark:bg-card">
-      @{new Date().getFullYear()} Tout droits réservés. 
+      Adam Bakir | Collège-Saint-Louis | Projet Personnel 2025-2026 | @{new Date().getFullYear()} Tout droits réservés. 
        <div className="opacity-70">
-        Ce site démontre un contenu éducatif uniquement. Il ne constitue pas un conseil financier.
+        Disclaimer: Ce site démontre un contenu éducatif uniquement. Il ne constitue pas un conseil financier.
       </div>
     </footer>
   );
